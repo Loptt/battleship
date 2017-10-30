6f9f0abce0a19a5b51905d740f9782018bdfbd88
vocemsapiens <vocem_sapiens@github.com>
2017-10-30 16:48:02 -0600

    Comments

--- Game.cpp
+++ Game.cpp
@@ -2,14 +2,16 @@
 MAIN FILE
 Erick Francisco González Martínez  A01039859
 Carlos Daniel Estrada Guerra       A01039919
-
+ BATTLESHIP_GAME
+ version 1.0
 */
 
 #include <iostream>
-#include "createdfunctions.h" //Own created libraries
+//Own created libraries
+#include "createdfunctions.h"
 using namespace std;
 
-//Battle space matrix size constant
+//Battle space matrix constant size
 const int iWidth =10;
 const int iHeight =10;
 
