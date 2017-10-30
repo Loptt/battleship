297296b731747ef5ae1a4be9f3b90b4ebeed94ce
vsapiens <31291605+vsapiens@users.noreply.github.com>
2017-10-30 13:09:04 -0500

    Update Ships.cpp

--- Ships.cpp
+++ Ships.cpp
@@ -3,22 +3,31 @@ using namespace std;
 
 struct Battleship {
 
-int iPositionX, iPositionY;
+int iPositionX, iPositionY,iLength;
+  
+  iLength = 5;
 };
 
 struct Cruiser {
 
-int iPositionX, iPositionY;
+int iPositionX, iPositionY, iLength;
+  
+  iLength = 4;
+  
 };
 
 struct Submarine {
   
-int iPositionX, iPositionY;
+int iPositionX, iPositionY, iLength;
+  
+  iLength = 3;
   
 };
 
 struct Destroyer {
 
-int iPositionX,iPositionY;
+int iPositionX, iPositionY,iLength;
+  
+   iLength = 3;
   
 };
