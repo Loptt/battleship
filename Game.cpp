c38b0591ed4ad1c1d4cf947bee3e66ea9da2cca1
vocemsapiens <vocem_sapiens@github.com>
2017-10-30 14:11:27 -0600

    Setup Game
    Create map

--- Game.cpp
+++ Game.cpp
@@ -1,1 +1,44 @@
+/*
+ Erick Francisco González Martínez  A01039859
+ Carlos Daniel Estrada Guerra       A01039919
+ 
+ 
+ 
+ 
+ */
+
+#include <iostream>
+
+using namespace std;
+
+bool bGameOver;
+
+const int iWidth =30;
+
+const int iHeight =30;
+
+int iPositionX, iPositionY;
+
+void Setup(){
+    
+    bGameOver= false;
+}
+
+void Input(){
+    
+    
+    
+    
+}
+
+
+int main(){
+    
+ 
+    
+    
+    
+}
+
+
 
