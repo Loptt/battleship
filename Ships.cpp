61105354903be975d271bc9d80c8e6f902731b5e
vsapiens <31291605+vsapiens@users.noreply.github.com>
2017-10-30 13:05:40 -0500

    Create Ships.cpp

---
+++ Ships.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+struct Destroyer {
+int iPositionX,iPositionY;
+};
+
+struct Destroyer {
+
+int iPositionX,iPositionY;
+};
+
+struct Destroyer {
+
+int iPositionX,iPositionY;
+};
+
+struct Destroyer {
+
+int iPositionX,iPositionY;
+};
