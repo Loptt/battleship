8c526418cd4274c89be738bf6b0c66df8f57223f
Loptt <carlos.deg02@hotmail.com>
2017-10-30 16:06:55 -0600

    Formatting Ships.cpp

--- Ships.cpp
+++ Ships.cpp
@@ -1,33 +1,27 @@
 #include <iostream>
 using namespace std;
 
-struct Battleship {
-
-int iPositionX, iPositionY,iLength;
-  
-  iLength = 5;
+struct Battleship
+{
+	int iPositionX, iPositionY,iLength;
+  	iLength = 5;
 };
 
-struct Cruiser {
-
-int iPositionX, iPositionY, iLength;
-  
-  iLength = 4;
-  
+struct Cruiser
+{
+	int iPositionX, iPositionY, iLength;
+  	iLength = 4; 
 };
 
-struct Submarine {
+struct Submarine
+{
   
-int iPositionX, iPositionY, iLength;
-  
-  iLength = 3;
-  
+	int iPositionX, iPositionY, iLength;
+  	iLength = 3;  
 };
 
-struct Destroyer {
-
-int iPositionX, iPositionY,iLength;
-  
-   iLength = 2;
-  
+struct Destroyer 
+{
+	int iPositionX, iPositionY,iLength;
+   	iLength = 2; 
 };
